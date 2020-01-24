93adf6c6a0a7bb2c2021c92303ab0c7d438151a8
Jayeson Brewster <jayeson.brewster@gmail.com>
2020-01-23 21:03:40 -0500

    Update column space

--- src/components/Creeds.js
+++ src/components/Creeds.js
@@ -6,12 +6,12 @@ const Creeds = () => {
         <div className='creeds mt-5'>
 
             <div className="row">
-                <div className="col-lg-4 col-md-6">
+                <div className="col-lg-4">
                     <h3>STUDENT CREED</h3>
                     <p>I intend to develop myself in a positive manner and avoid anything that will reduce my mental growth or physical health. I intend to develop self-discipline in order to bring out the best in myself and others. I intend to use what I learn in class constructively and defensively to help myself and my fellow man and never to be abusive or offensive.</p>
 
                 </div>
-                <div className="col-lg-4 col-md-6">
+                <div className="col-lg-4">
                     <h3>THE KARATE CREED</h3>
                     <p>I come to you with only KARATE, my empty hands, I have no weapons. But should I be forced to defend myself, my honor or my principles, should it be at matter of life or death, of right or wrong, then here are my weapons, KARATE, my empty hands.</p>
 
